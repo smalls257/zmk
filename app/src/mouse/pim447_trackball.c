5379d88b6a398688c1c44577ae32c9340668d202
Jason smith <smalls257@outlook.com>
2021-11-10 14:43:52 -0500

    Update pim447_trackball.c

--- app/src/mouse/pim447_trackball.c
+++ app/src/mouse/pim447_trackball.c
@@ -3,16 +3,11 @@
  *
  * SPDX-License-Identifier: MIT
  */
-#include <zephyr.h>
-#include <device.h>
-#include <devicetree.h>
-#include <zmk/mouse.h>
-
-
-#include <drivers/i2c.h>
 
 #include <drivers/sensor.h>
 #include <logging/log.h>
+
+#include <drivers/i2c.h>
 
 LOG_MODULE_REGISTER(PIM447, CONFIG_SENSOR_LOG_LEVEL);
 
@@ -47,6 +42,73 @@ static void thread_code(void *p1, void *p2, void *p3)
     const struct device *dev;
     int result;
 
+    /* PIM447 trackball initialization. */
+
+   // const char *label = DT_LABEL(DT_INST(0, pimoroni_pim447_trackball));
+    dev = device_get_binding(DT_INST_REG_ADDR(0));
+    if (dev == NULL) {
+        LOG_ERR("Cannot get PIM447_TRACKBALL device");
+        return;
+    }
+
+    /* Event loop. */
+
+    bool button_press_sent   = false;
+    bool button_release_sent = false;
+
+    while (true) {
+        struct sensor_value pos_dx, pos_dy, pos_dz;
+        bool send_report = false;
+
+        result = sensor_sample_fetch(dev);
+        if (result < 0) {
+            LOG_ERR("Failed to fetch PIM447_TRACKBALL sample");
+            return;
+        }
+
+        result = sensor_channel_get(dev, SENSOR_CHAN_POS_DX, &pos_dx);
+        if (result < 0) {
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dx channel value");
+            return;
+        }
+
+        result = sensor_channel_get(dev, SENSOR_CHAN_POS_DY, &pos_dy);
+        if (result < 0) {
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dy channel value");
+            return;
+        }
+
+        result = sensor_channel_get(dev, SENSOR_CHAN_POS_DZ, &pos_dz);
+        if (result < 0) {
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dz channel value");
+            return;
+        }
+
+        if (pos_dx.val1 != 0 || pos_dy.val1 != 0) {
+            zmk_hid_mouse_movement_update(convert_speed(pos_dx.val1),
+                                          convert_speed(pos_dy.val1));
+            send_report = true;
+        }
+
+        if (pos_dz.val1 == 0x80 && button_press_sent == false) {
+            zmk_hid_mouse_button_press(0);
+            button_press_sent   = true;
+            button_release_sent = false;
+            send_report = true;
+        } else if (pos_dz.val1 == 0x01 && button_release_sent == false) {
+            zmk_hid_mouse_button_release(0);
+            button_press_sent   = false;
+            button_release_sent = true;
+            send_report = true;
+        }
+
+        if (send_report) {
+            zmk_endpoints_send_mouse_report();
+            zmk_hid_mouse_clear();
+        }
+
+        k_sleep(K_MSEC(10));
+    }
 }
 
 #define STACK_SIZE 1024

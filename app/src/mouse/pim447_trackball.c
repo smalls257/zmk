562ff7d6da2352c17f6417fa6539ab864778e54d
Jason smith <smalls257@outlook.com>
2021-11-22 16:17:19 -0500

    Update pim447_trackball.c

--- app/src/mouse/pim447_trackball.c
+++ app/src/mouse/pim447_trackball.c
@@ -6,72 +6,27 @@
 
 #include <drivers/sensor.h>
 #include <logging/log.h>
-#include <zmk/hid.h>
-#include <zmk/endpoints.h>
-#include <zmk/trackball_pim447.h>
 
 LOG_MODULE_REGISTER(PIM447, CONFIG_SENSOR_LOG_LEVEL);
 
-#define MOVE_FACTOR    DT_PROP(DT_INST(0, pimoroni_trackball_pim447), move_factor)
-#define MOVE_X_INVERT  DT_PROP(DT_INST(0, pimoroni_trackball_pim447), invert_move_x)
-#define MOVE_Y_INVERT  DT_PROP(DT_INST(0, pimoroni_trackball_pim447), invert_move_y)
-#define MOVE_X_FACTOR  (MOVE_FACTOR * (MOVE_X_INVERT ? -1 : 1))
-#define MOVE_Y_FACTOR  (MOVE_FACTOR * (MOVE_Y_INVERT ? -1 : 1))
-
-#define SCROLL_DIVISOR    DT_PROP(DT_INST(0, pimoroni_trackball_pim447), scroll_divisor)
-#define SCROLL_X_INVERT   DT_PROP(DT_INST(0, pimoroni_trackball_pim447), invert_scroll_x)
-#define SCROLL_Y_INVERT   DT_PROP(DT_INST(0, pimoroni_trackball_pim447), invert_scroll_y)
-#define SCROLL_X_DIVISOR  (SCROLL_DIVISOR * (SCROLL_X_INVERT ? -1 : 1))
-#define SCROLL_Y_DIVISOR  (SCROLL_DIVISOR * (SCROLL_Y_INVERT ?  1 : -1))
-
-#define BUTTON    DT_PROP(DT_INST(0, pimoroni_trackball_pim447), button)
-#define SWAP_AXES DT_PROP(DT_INST(0, pimoroni_trackball_pim447), swap_axes)
-
-static int mode = DT_PROP(DT_INST(0, pimoroni_trackball_pim447), mode);
-
-void zmk_trackball_pim447_set_mode(int new_mode)
-{
-    switch (new_mode) {
-        case PIM447_MOVE:
-        case PIM447_SCROLL:
-            mode = new_mode;
-            break;
-
-       case PIM447_TOGGLE:
-            mode = mode == PIM447_MOVE
-                   ? PIM447_SCROLL
-                   : PIM447_MOVE;
-            break;
-
-       default:
-            break;
-    }
-}
-
 /*
- * It feels more natural and more confortable to convert the speed
+ * It feels more natural and more confortable to square the speed
  * reported by the PIM447 trackball.
  */
 static int16_t convert_speed(int32_t value)
 {
-    bool negative = (value < 0);
+    bool negative = false;
 
-    if (negative) {
+    if (value < 0) {
+        negative = true;
         value = -value;
     }
 
-    switch (value) {
-        case 0:  value = 0;   break;
-        case 1:  value = 1;   break;
-        case 2:  value = 4;   break;
-        case 3:  value = 8;   break;
-        case 4:  value = 18;  break;
-        case 5:  value = 32;  break;
-        case 6:  value = 50;  break;
-        case 7:  value = 72;  break;
-        case 8:  value = 98;  break;
-        default: value = 127; break;
+    if (value > 11) {
+        value = 11;
     }
+
+    value = value * value;
 
     if (negative) {
         value = -value;
@@ -87,21 +42,26 @@ static void thread_code(void *p1, void *p2, void *p3)
 
     /* PIM447 trackball initialization. */
 
-  const char *label = DT_LABEL(DT_INST(0, pimoroni_pim447_trackball));
-
+    const char *label = DT_LABEL(DT_INST(0, pimoroni_pim447_trackball));
+    
     int i = 0;
-
+    
     while (i <30)
     {
         dev = device_get_binding(label);
-
+        
         if (dev == NULL) {
             LOG_ERR("Cannot get PIM447_TRACKBALL device");
         }
-
+        
         k_sleep(K_MSEC(1000));
         i++;
     }
+    
+    if (dev == NULL) {
+            LOG_ERR("Cannot get PIM447_TRACKBALL device");
+        return;
+        }
 
     /* Event loop. */
 
@@ -111,68 +71,44 @@ static void thread_code(void *p1, void *p2, void *p3)
     while (true) {
         struct sensor_value pos_dx, pos_dy, pos_dz;
         bool send_report = false;
-        int clear = PIM447_NONE;
 
         result = sensor_sample_fetch(dev);
         if (result < 0) {
-            LOG_ERR("Failed to fetch TRACKBALL_PIM447 sample");
+            LOG_ERR("Failed to fetch PIM447_TRACKBALL sample");
             return;
         }
 
         result = sensor_channel_get(dev, SENSOR_CHAN_POS_DX, &pos_dx);
         if (result < 0) {
-            LOG_ERR("Failed to get TRACKBALL_PIM447 pos_dx channel value");
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dx channel value");
             return;
         }
 
         result = sensor_channel_get(dev, SENSOR_CHAN_POS_DY, &pos_dy);
         if (result < 0) {
-            LOG_ERR("Failed to get TRACKBALL_PIM447 pos_dy channel value");
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dy channel value");
             return;
         }
 
         result = sensor_channel_get(dev, SENSOR_CHAN_POS_DZ, &pos_dz);
         if (result < 0) {
-            LOG_ERR("Failed to get TRACKBALL_PIM447 pos_dz channel value");
+            LOG_ERR("Failed to get PIM447_TRACKBALL pos_dz channel value");
             return;
         }
 
         if (pos_dx.val1 != 0 || pos_dy.val1 != 0) {
-            if (SWAP_AXES) {
-                int32_t tmp = pos_dx.val1;
-                pos_dx.val1 = pos_dy.val1;
-                pos_dy.val1 = tmp;
-            }
-
-            switch(mode) {
-                default:
-                case PIM447_MOVE: {
-                    int dx = convert_speed(pos_dx.val1) * MOVE_X_FACTOR;
-                    int dy = convert_speed(pos_dy.val1) * MOVE_Y_FACTOR;
-                    zmk_hid_mouse_movement_set(dx, dy);
-                    send_report = true;
-                    clear = PIM447_MOVE;
-                    break;
-                }
-
-                case PIM447_SCROLL: {
-                    int dx = pos_dx.val1 / SCROLL_X_DIVISOR;
-                    int dy = pos_dy.val1 / SCROLL_Y_DIVISOR;
-                    zmk_hid_mouse_scroll_set(dx, dy);
-                    send_report = true;
-                    clear = PIM447_SCROLL;
-                    break;
-                }
-            }
+            zmk_hid_mouse_movement_update(convert_speed(pos_dx.val1),
+                                          convert_speed(pos_dy.val1));
+            send_report = true;
         }
 
         if (pos_dz.val1 == 0x80 && button_press_sent == false) {
-            zmk_hid_mouse_button_press(BUTTON);
+            zmk_hid_mouse_button_press(0);
             button_press_sent   = true;
             button_release_sent = false;
             send_report = true;
         } else if (pos_dz.val1 == 0x01 && button_release_sent == false) {
-            zmk_hid_mouse_button_release(BUTTON);
+            zmk_hid_mouse_button_release(0);
             button_press_sent   = false;
             button_release_sent = true;
             send_report = true;
@@ -180,12 +116,7 @@ static void thread_code(void *p1, void *p2, void *p3)
 
         if (send_report) {
             zmk_endpoints_send_mouse_report();
-
-            switch (clear) {
-                case PIM447_MOVE: zmk_hid_mouse_movement_set(0, 0); break;
-                case PIM447_SCROLL: zmk_hid_mouse_scroll_set(0, 0); break;
-                default: break;
-            }
+            zmk_hid_mouse_clear();
         }
 
         k_sleep(K_MSEC(10));
@@ -197,11 +128,11 @@ static void thread_code(void *p1, void *p2, void *p3)
 static K_THREAD_STACK_DEFINE(thread_stack, STACK_SIZE);
 static struct k_thread thread;
 
-int zmk_trackball_pim447_init()
+int zmk_pim447_trackball_init()
 {
     k_thread_create(&thread, thread_stack, STACK_SIZE, thread_code,
                     NULL, NULL, NULL, K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
     return 0;
 }
 
-SYS_INIT(zmk_trackball_pim447_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(zmk_pim447_trackball_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
